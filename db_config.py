3aad056e31630c085faf744e6c56a13efb94a57f
Uday <udaysrivastav04@gmail.com>
2026-04-16 02:22:51 +0530

    indent fix

--- db_config.py
+++ db_config.py
@@ -5,9 +5,12 @@ from flask_mysqldb import MySQL
 def init_db(app):
     # Set MYSQL_PASSWORD (and optionally other vars) as environment variables,
     # or edit the defaults below for local development only.
-    app.config['MYSQL_HOST']        = os.environ.get('MYSQL_HOST', 'localhost')
-    app.config['MYSQL_USER']        = os.environ.get('MYSQL_USER', 'root')
-    app.config['MYSQL_PASSWORD']    = os.environ.get('MYSQL_PASSWORD', 'root@bs6SQL')
-    app.config['MYSQL_DB']          = os.environ.get('MYSQL_DB', 'finance_advisor')
+    def init_db(app):
+     app.config['MYSQL_HOST'] = 'nozomi.proxy.rlwy.net'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'ZSKWPSzaiEiqKDJmMlJQxKLdNbwdhglO'
+    app.config['MYSQL_DB'] = 'railway'
+    app.config['MYSQL_PORT'] = 20430
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    
     return MySQL(app)
