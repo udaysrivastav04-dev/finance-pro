81e5359ebc20a311f4a5bf2b91c3125bc6d106a2
Uday <udaysrivastav04@gmail.com>
2026-04-16 03:47:29 +0530

    fix db password

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 def init_db(app):
     app.config['MYSQL_HOST'] = 'nozomi.proxy.rlwy.net'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'ZSkWPszaeiIqKDJmM1JQxKLdNbwdhg10'
+    app.config['MYSQL_PASSWORD'] = 'ZSKWPSzaiEiqKDJmMlJQxKLdNbwdhglO'
     app.config['MYSQL_DB'] = 'railway'
     app.config['MYSQL_PORT'] = 20430
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
