5503e8fc1c90404cba2a403f0ae1256d498b688b
Uday <udaysrivastav04@gmail.com>
2026-04-16 02:03:52 +0530

    deploy change

--- app.py
+++ app.py
@@ -455,4 +455,4 @@ def home():
     return jsonify({'message': 'Finance Advisor Backend Running ✅'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
